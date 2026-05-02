27271a5834e6218f7e50acc0ae05cb087d1710ab
Ezcode_Hung <nguyenphuchung27062012@gmail.com>
2026-05-02 17:56:51 +0700

    init

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,8 +1,6 @@
-cat > src/bot/__init__.py << 'ENDOFFILE'
+
 from .client import FlareBot
 from .events import setup_events
 
 __all__ = ["FlareBot", "setup_events"]
-ENDOFFILE
 
-echo "✅ bot __init__.py fixed!"
